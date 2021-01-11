8f805d585ad3e582deaca00ede9282d97ce8054c
christa-cheung <54732090+christa-cheung@users.noreply.github.com>
2021-01-11 09:41:59 -0800

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+
+const app: express.Application = express()
+const address: string = "0.0.0.0:3000"
+
+app.use(bodyParser.json())
+
+app.get('/', function (req: Request, res: Response) {
+    res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+    console.log(`starting app on: ${address}`)
+})
