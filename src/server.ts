09467e19bdc82b11015fc896b191d0e505d3054d
Ahmed Ragab <ahmedasiawy@gmail.com>
2022-03-09 13:22:14 +0200

    config eslint with prettier.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const address = "0.0.0.0:3000";
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+app.get("/", function (req: Request, res: Response) {
+	res.send("Hello World!");
+});
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+	console.log(`starting app on: ${address}`);
+});
