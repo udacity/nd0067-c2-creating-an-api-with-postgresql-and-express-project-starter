37a95838858008fb645fe8e653275664e51e3e9f
drichards188 <github@cxoprotect.com>
2022-11-29 16:48:48 -0700

    first tests working

--- src/tests/productIndexSpec.ts
+++ src/tests/productIndexSpec.ts
@@ -5,12 +5,12 @@ const store = new ProductStore();
 describe('Product model', () => {
     it('index method should return a list of products', async () => {
         const result = await store.index();
-        expect(result).toContain({ id: 1, name: 'ipad', price: 1000 });
+        expect(result).toContain({ id: 1, name: 'cake', price: 58 });
     });
 
     it('single method should return a product', async () => {
         const result = await store.single(1);
-        expect(result).toContain({ id: 1, name: 'ipad', price: 1000 });
+        expect(result).toContain({ id: 1, name: 'cake', price: 58 });
     });
 
     it('create method should return a count of 1', async () => {
