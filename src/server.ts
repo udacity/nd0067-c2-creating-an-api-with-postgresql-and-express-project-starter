cc112b952cd51b10cbbdf8125a842695c0bbbdcb
djasnive <djasniveyrajaona@gmail.com>
2022-12-29 21:36:45 +0300

    Add cors to the server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import {router} from './routes/index.router'
+import cors from 'cors'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 
 app.use(router)
