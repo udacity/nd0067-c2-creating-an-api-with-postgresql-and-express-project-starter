7eb7bef8ac09200ac7a57ae8e74415eb47d5bcae
farahmahmoud70 <farah.mahmoud7080@gmail.com>
2023-02-07 18:21:48 +0200

    [feat]: setup environment

---
+++ src/database.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV,
+} = process.env;
+
+let Client;
+console.log(ENV);
+
+if (ENV === "test") {
+  Client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+if (ENV === "dev") {
+  Client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+export default Client;
